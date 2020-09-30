235d4a7abb0c053a098abb929d4bff76e4908d56
Aarron Stephenson <caleb.stephenson@live.co.uk>
2020-09-30 15:33:06 +0100

    Reanamed some things to PascalCasing, and made interface which defined by doesn't implement commands

---
+++ apipractice/Data/IApiPracticeRepo.cs
@@ -0,0 +1,14 @@
+﻿using ApiPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPractice.Data
+{
+    interface IApiPracticeRepo
+    {
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int Id);
+    }
+}
--- apipractice/Program.cs
+++ apipractice/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace apipractice
+namespace ApiPractice
 {
     public class Program
     {
