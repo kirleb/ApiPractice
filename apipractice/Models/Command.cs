db0c8beab1ef66af19c7dc79d11edd9e2858cc02
Aarron Stephenson <caleb.stephenson@live.co.uk>
2020-09-30 14:59:14 +0100

    Made model command.cs

--- apipractice/Models/Command.cs
+++ apipractice/Models/Command.cs
@@ -7,5 +7,9 @@ namespace apipractice.Models
 {
     public class Command
     {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
     }
 }
