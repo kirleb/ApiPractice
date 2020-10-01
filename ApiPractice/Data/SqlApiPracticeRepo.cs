eacef2dfa8e38c5faa8d931948a130929d617dad
Aarron Stephenson <caleb.stephenson@live.co.uk>
2020-10-01 15:54:34 +0100

    SqlApiPracticeRepo updated code and startup change mock repo to sql repo

---
+++ ApiPractice/Data/SqlApiPracticeRepo.cs
@@ -0,0 +1,25 @@
+﻿using ApiPractice.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiPractice.Data
+{
+    public class SqlApiPracticeRepo : IApiPracticeRepo
+    {
+        private readonly ApiPracticeContext _context;
+
+        public SqlApiPracticeRepo(ApiPracticeContext context)
+        {
+            _context = context;
+        } 
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.Commands.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.Commands.FirstOrDefault(p => p.Id == id); //Id is the id in Commands id is the parameter
+        }
+    }
+}
