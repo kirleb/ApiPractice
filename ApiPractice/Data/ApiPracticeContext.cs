18c9640c659aaae9da365677fe392b09a694b66b
Aarron Stephenson <caleb.stephenson@live.co.uk>
2020-10-01 14:07:05 +0100

    csproj connected to user sercrets, and apipracticecontext made

---
+++ ApiPractice/Data/ApiPracticeContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiPractice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPractice.Data
+{
+    public class ApiPracticeContext : DbContext
+    {
+        public ApiPracticeContext(DbContextOptions<ApiPracticeContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
