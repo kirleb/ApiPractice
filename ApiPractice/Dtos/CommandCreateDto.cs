b19c4b17e785cb0fd77e89da99f876f66b9c8372
Aarron Stephenson <caleb.stephenson@live.co.uk>
2020-10-02 10:46:45 +0100

    Added data annotations to CommandCreateDto so a 400 error is given instead of a 500

--- ApiPractice/Dtos/CommandCreateDto.cs
+++ ApiPractice/Dtos/CommandCreateDto.cs
@@ -1,11 +1,17 @@
-﻿namespace ApiPractice.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPractice.Dtos
 {
     public class CommandCreateDto
     {
+        [Required]
+        [MaxLength(80)]
         public string HowTo { get; set; }
 
+        [Required]
         public string Line { get; set; }
 
+        [Required]
         public string Platform { get; set; }
     }
 }
